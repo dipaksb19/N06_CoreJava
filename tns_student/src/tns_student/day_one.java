dc0c43465f403d3be48dd56bedeb95a9d9250a1e
Lenovo <Lenovo@DESKTOP-IOTOQ92>
2023-07-20 14:26:29 +0530

    commit_one

---
+++ tns_student/src/tns_student/day_one.java
@@ -0,0 +1,13 @@
+//Day 1 of core java
+// program to demonstrate on compile time input
+package tns_student;
+
+public class day_one {
+		public static void main(String [] args) {
+			System.out.println("****Day One****");
+			
+			//Variable initialization
+			short num = 34;
+			System.out.println("The num is " + num);
+		}
+}
---
+++ tns_student/src/tns_student/user_input.java
@@ -0,0 +1,20 @@
+package tns_student;
+
+import java.util.Scanner;
+
+// Program to demonstate on user input
+public class user_input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		String str;
+		System.out.println("Enter the string : ");
+		str = in.nextLine();
+		System.out.println("Given String : " + str);
+		//s.close()
+
+	}
+
+}
