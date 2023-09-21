f0fbe07ef965b41ed563a382ea07050249cb4d43
Lenovo <Lenovo@DESKTOP-IOTOQ92>
2023-09-21 22:35:06 +0530

    First Commit

---
+++ day_fourteen/src/org/tnsif/dayfourteen/synchronization/InsufficientBalanceException.java
@@ -0,0 +1,12 @@
+package org.tnsif.dayfourteen.synchronization;
+
+public class InsufficientBalanceException extends Exception{
+	public InsufficientBalanceException()
+	{
+		super("Can't Withdraw...Insufficient balance");
+	}
+	public InsufficientBalanceException(String message)
+	{
+		super(message);
+	}
+}
